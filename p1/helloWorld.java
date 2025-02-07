af2440d95ade8fbea0582f5e4069dd894d9ce463
NathanDSanta <natan.d.santa@gmail.com>
2025-02-07 09:28:59 +0100

    making the first code a little more complex

--- p1/helloWorld.java
+++ p1/helloWorld.java
@@ -1,5 +1,10 @@
 public class helloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World!");
+    int i = 10;
+    while (i > 0) {
+      System.out.println(i);
+      i--;
+    }
   }
 }
